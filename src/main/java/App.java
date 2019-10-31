569afec0970701026dabc3966e84c4465b779100
Brian Odida <bryodiiidah@gmail.com>
2019-10-31 10:45:23 +0300

    adds a route to the landing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,17 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 
 public class App {
         public static void main(String[] args){
-
+                get("/", (req, res)->{
+                        Map<String, Object> model = new HashMap<String, Object>();
+                        return new ModelAndView(model, "index.hbs");
+                }, new HandlebarsTemplateEngine());
 
         }
 
