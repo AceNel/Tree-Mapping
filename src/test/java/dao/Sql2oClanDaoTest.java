1455cff90aa2863947c5245fcdf4d401db18d4a6
Nelson Kimani <nelsonkimani96@gmail.com>
2019-10-31 15:49:36 +0300

    Update sql20 a clan class test

---
+++ src/test/java/dao/Sql2oClanDaoTest.java
@@ -0,0 +1,60 @@
+package dao;
+
+import models.Clan;
+import models.Users;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oClanDaoTest {
+    private Sql2oUserDao userDao = new Sql2oUserDao();
+    private Sql2oClanDao clanDao = new Sql2oClanDao();
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    @Test
+    public void getClanId_clanIsInsertedIntoDatabase_int() throws Exception {
+        Clan clan = newClan();
+        System.out.println( clan.getClanId());
+        assertNotEquals(0, clan.getClanId());
+    }
+    @Test
+    public void getClanName() throws Exception{
+        Clan clan = newClan();
+        assertEquals("Area 51", clan.getClanName());
+    }
+
+    @Test
+    public void getClanDescription() throws Exception{
+        Clan clan = newClan();
+        assertEquals("We lead others follow",clan.getClanDescription());
+    }
+
+
+
+
+    //Helper
+    private Clan newClan() throws Exception{
+        Clan clan = new Clan("Area 51",1,"We lead others follow");
+        clanDao.add(clan);
+        return clan;
+    }
+
+    //Helper
+    private Users newUser() throws Exception{
+        Users user = new Users("username","asdf");
+        userDao.add(user);
+        return user;
+    }
+
+    private Users newUser2() throws Exception {
+        Users user = new Users("anotherusername","1234");
+        userDao.add(user);
+        return user;
+    }
+
+
+
+}
